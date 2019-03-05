dd22cf331d2dfc7dbfac91e03c39e72879cfba36
Alex Sasser and Jen Bruce <alex@>
2019-03-04 18:30:12 -0800

    add initial HTML, CSS, JS, and jquery and bootstrap.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Business Logic
+
+
+
+// User Logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
